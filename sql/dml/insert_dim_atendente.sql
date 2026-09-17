28aca0e221b65e14e46daf2869057fd009d5deb5
Paloma <palomavdsilva@outlook.com>
2026-09-17 17:27:33 -0300

    Adiciona dml das tabelas dimensões

---
+++ sql/dml/insert_dim_atendente.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (1,'Lucas Andrade','Nível 1');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (2,'Mariana Costa','Nível 1');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (3,'Carlos Eduardo Rocha','Nível 1');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (4,'Beatriz Souza','Nível 1');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (5,'Fernanda Lima','Nível 1');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (6,'Gabriel Martins','Nível 2');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (7,'Camila Alves','Nível 2');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (8,'Rafael Oliveira','Nível 2');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (9,'Juliana Mendes','Nível 2');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (10,'Thiago Barbosa','Nível 3');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (11,'Vanessa Ribeiro','Nível 3');
+INSERT INTO `` (`id_atendente`,`nome`,`nivel`) VALUES (12,'Rodrigo Santos','Nível 3');
---
+++ sql/dml/insert_dim_categoria_chamado.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `` (`id_categoria_chamado`,`descricao`,`prazo_maximo`) VALUES (1,'Sem Conexão / Sinal Queda',240);
+INSERT INTO `` (`id_categoria_chamado`,`descricao`,`prazo_maximo`) VALUES (2,'Lentidão na Conexão',180);
+INSERT INTO `` (`id_categoria_chamado`,`descricao`,`prazo_maximo`) VALUES (3,'Troca de Senha / Wi-Fi',30);
+INSERT INTO `` (`id_categoria_chamado`,`descricao`,`prazo_maximo`) VALUES (4,'Segunda Via / Faturamento',45);
+INSERT INTO `` (`id_categoria_chamado`,`descricao`,`prazo_maximo`) VALUES (5,'Mudança de Endereço',1440);
+INSERT INTO `` (`id_categoria_chamado`,`descricao`,`prazo_maximo`) VALUES (6,'Upgrade / Downgrade de Plano',120);
+INSERT INTO `` (`id_categoria_chamado`,`descricao`,`prazo_maximo`) VALUES (7,'Defeito no Roteador / Equipamento',480);
